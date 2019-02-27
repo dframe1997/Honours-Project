726371949cdb8dc40a332c4c6a4b5a159dbdae1e
David Frame <40200819@live.napier.ac.uk>
2019-02-27 22:35:46 +0000

    Started experimenting with NLTK

---
+++ Experiments/NLTKExperiments.py
@@ -0,0 +1,19 @@
+import nltk
+
+nltk.data.path.append(r"D:\Users\David\Documents\Work\University\Year 4\Honours\NLTK")
+
+sentence = """At eight o'clock on Thursday morning
+... Arthur didn't feel very good."""
+
+tokens = nltk.word_tokenize(sentence)
+print (tokens)
+
+tagged = nltk.pos_tag(tokens)
+print (tagged[0:6])
+
+entities = nltk.chunk.ne_chunk(tagged)
+print(entities)
+
+from nltk.corpus import treebank
+t = treebank.parsed_sents('wsj_0001.mrg')[0]
+t.draw()
